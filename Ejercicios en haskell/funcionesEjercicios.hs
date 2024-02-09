a6673bc378066df4b411fdd07dcdcd02d0e90ba9
OscarC2010389 <c20120389@alumnos.itsur.edu.mx>
2024-02-08 21:16:13 -0600

    "Actualización numero 1"

--- Ejercicios en haskell/funcionesEjercicios.hs
+++ Ejercicios en haskell/funcionesEjercicios.hs
@@ -13,9 +13,15 @@ module Funciones where
     aplicarIVA :: Porcentaje -> Precio -> Precio
     aplicarIVA iva precio = precio + (precio * iva / 100)
 
+    -- Función que recibe un diccionario con precios y porcentajes de una cesta de la compra,
+    -- y una función para aplicar descuentos o IVA, y devuelve el precio final de la cesta
+    calcularPrecioFinal :: [(Float, Float)] -> (Float -> Float -> Float) -> Float
+    calcularPrecioFinal cesta funcion = sum [funcion precio porcentaje | (precio, porcentaje) <- cesta]
+
     main :: IO ()
     main = do
         let precioOriginal = 100.0
+        let cantidadDeIVA = 16.0
         -- let descuento = 10.0
 
         -- let precioConDescuento = aplicarDescuento descuento precioOriginal
@@ -24,3 +30,19 @@ module Funciones where
         -- putStrLn $ "Descuento aplicado: " ++ show descuento ++ "%"
         -- putStrLn $ "Precio con descuento: " ++ show precioConDescuento
 
+        -- let precioConIVA = aplicarIVA cantidadDeIVA precioOriginal
+
+        -- putStrLn $ "Precio original: " ++ show precioOriginal
+        -- putStrLn $ "IVA aplicado: " ++ show cantidadDeIVA ++ "%"
+        -- putStrLn $ "Precio con IVA: " ++ show precioConIVA
+
+        let cestaDeCompra = [(2.0, 10), (3.0, 5), (5.0, 8)] -- Lista (porcentaje, precio)
+    
+        -- Calcular el precio final aplicando un descuento del 10%
+        let precioFinalDescuento = calcularPrecioFinal cestaDeCompra (\porcentaje precio -> aplicarDescuento porcentaje precio)
+        putStrLn $ "Precio final con descuento: " ++ show precioFinalDescuento
+        
+        -- Calcular el precio final aplicando el IVA del 16%
+        let precioFinalIVA = calcularPrecioFinal cestaDeCompra (\porcentaje precio -> aplicarIVA cantidadDeIVA precio)
+        putStrLn $ "Precio final con IVA: " ++ show precioFinalIVA
+        
